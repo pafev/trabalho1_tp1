1ca4f086a7b1a4db7560f944516512dc50eea8d1
pafev <paulofvilarimda@gmail.com>
2023-05-06 02:13:13 -0300

    modelo testes

--- testes.cpp
+++ testes.cpp
@@ -1,46 +1,5 @@
 #include "testes.hpp"
 
-// void TesteDominio::setUp() {
-//     dominio = new Dominio();
-//     estado = SUCESSO;
-// };
-
-// void TesteDominio::tearDown() {
-//     delete dominio;
-// };
-
-// void TesteDominio::testarValorInvalido(string valor) {
-//     try {
-//         dominio->setValor(valor);
-//         estado = FALHA + "Valor invalido setado";
-//     }
-//     catch(invalid_argument &excecao) {
-//         if (dominio->getValor() == valor) {
-//             estado = FALHA + excecao.what();
-//         };
-//     };
-// };
-
-// void TesteDominio::testarValorValido(string valor) {
-//     try {
-//         dominio->setValor(valor);
-//         if (dominio->getValor() != valor) {
-//             estado = FALHA + "Valor valido não foi setado";
-//         };
-//     }
-//     catch(invalid_argument &excecao) {
-//         estado = FALHA + excecao.what();
-//     };
-// };
-
-// string TesteDominio::run(string valorInvalido, string valorValido) {
-//     setUp();
-//     testarValorInvalido(valorInvalido);
-//     testarValorValido(valorValido);
-//     tearDown();
-//     return estado;
-// }
-
 void TesteData::setUp() {
     data = new Data();
     estado = SUCESSO;
@@ -75,6 +34,47 @@ void TesteData::testarValorValido(string valor) {
 };
 
 string TesteData::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+}
+
+void TesteSenha::setUp() {
+    senha = new Senha();
+    estado = SUCESSO;
+};
+
+void TesteSenha::tearDown() {
+    delete senha;
+};
+
+void TesteSenha::testarValorInvalido(string valor) {
+    try {
+        senha->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (senha->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+
+void TesteSenha::testarValorValido(string valor) {
+    try {
+        senha->setValor(valor);
+        if (senha->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+
+string TesteSenha::run(string valorInvalido, string valorValido) {
     setUp();
     testarValorInvalido(valorInvalido);
     testarValorValido(valorValido);
--- testes.hpp
+++ testes.hpp
@@ -23,4 +23,20 @@ public:
     string run(string, string);
 };
 
+class TesteSenha {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Senha *senha;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
+
 #endif // TESTES_HPP_INCLUDED
