686ecc966732a9dd264e5c89ea5f7527b7ace710
pafev <paulofvilarimda@gmail.com>
2023-05-06 01:23:03 -0300

    modelo testes

--- testes.cpp
+++ testes.cpp
@@ -1,1 +1,83 @@
 #include "testes.hpp"
+
+// void TesteDominio::setUp() {
+//     dominio = new Dominio();
+//     estado = SUCESSO;
+// };
+
+// void TesteDominio::tearDown() {
+//     delete dominio;
+// };
+
+// void TesteDominio::testarValorInvalido(string valor) {
+//     try {
+//         dominio->setValor(valor);
+//         estado = FALHA + "Valor invalido setado";
+//     }
+//     catch(invalid_argument &excecao) {
+//         if (dominio->getValor() == valor) {
+//             estado = FALHA + excecao.what();
+//         };
+//     };
+// };
+
+// void TesteDominio::testarValorValido(string valor) {
+//     try {
+//         dominio->setValor(valor);
+//         if (dominio->getValor() != valor) {
+//             estado = FALHA + "Valor valido não foi setado";
+//         };
+//     }
+//     catch(invalid_argument &excecao) {
+//         estado = FALHA + excecao.what();
+//     };
+// };
+
+// string TesteDominio::run(string valorInvalido, string valorValido) {
+//     setUp();
+//     testarValorInvalido(valorInvalido);
+//     testarValorValido(valorValido);
+//     tearDown();
+//     return estado;
+// }
+
+void TesteData::setUp() {
+    data = new Data();
+    estado = SUCESSO;
+};
+
+void TesteData::tearDown() {
+    delete data;
+};
+
+void TesteData::testarValorInvalido(string valor) {
+    try {
+        data->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (data->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+
+void TesteData::testarValorValido(string valor) {
+    try {
+        data->setValor(valor);
+        if (data->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+
+string TesteData::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+}
--- testes.hpp
+++ testes.hpp
@@ -1,6 +1,26 @@
 #ifndef TESTES_HPP_INCLUDED
 #define TESTES_HPP_INCLUDED
 
+#include <string>
+#include <stdexcept>
+using namespace std;
+#include "dominios.hpp"
+#include "entidades.hpp"
 
+class TesteData {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Data *data;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
 
 #endif // TESTES_HPP_INCLUDED
