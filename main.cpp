d4e80c1428986ae5c6aa9f5ec4a731ad846f8526
pafev <paulofvilarimda@gmail.com>
2023-05-05 22:48:25 -0300

    voltando ao padrão

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include "dominios/dominios.hpp"
-#include "entidades/entidades.hpp"
-#include "testes/testes.hpp"
+#include "dominios.hpp"
+#include "entidades.hpp"
+#include "testes.hpp"
 
 #include <iostream>
 using namespace std;
@@ -10,15 +10,5 @@ int main() {
     x.setValor("30/FEV/2004");
     cout << x.getValor() << endl;
 
-    Data z;
-    z.setValor("01/DEZ/2999");
-    cout << z.getValor() << endl;
-
-    return 0;
-
-    Data y;
-    y.setValor("20/MAI/2004");
-
-    cout << y.getValor() << endl;
     return 0;
 }
