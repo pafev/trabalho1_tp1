3dc3d473d2777226d4d6e0c6bfdf1bb03189916a
jessica8melo <jessica8melo@gmail.com>
2023-05-06 02:45:36 -0300

    ✅ adicionados testes de todos os domínios

--- testes.cpp
+++ testes.cpp
@@ -122,4 +122,226 @@ string TesteSenha::run(string valorInvalido, string valorValido) {
     testarValorValido(valorValido);
     tearDown();
     return estado;
-}
+};
+
+void TesteClasse::setUp() {
+    classe = new Classe();
+    estado = SUCESSO;
+};
+void TesteClasse::tearDown() {
+    delete classe;
+};
+void TesteClasse::testarValorInvalido(string valor) {
+    try {
+        classe->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (classe->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+void TesteClasse::testarValorValido(string valor) {
+    try {
+        classe->setValor(valor);
+        if (classe->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+string TesteClasse::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+};
+
+void TesteTelefone::setUp() {
+    telefone = new Telefone();
+    estado = SUCESSO;
+};
+void TesteTelefone::tearDown() {
+    delete telefone;
+};
+void TesteTelefone::testarValorInvalido(string valor) {
+    try {
+        telefone->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (telefone->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+void TesteTelefone::testarValorValido(string valor) {
+    try {
+        telefone->setValor(valor);
+        if (telefone->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+string TesteTelefone::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+};
+
+void TesteCodigo::setUp() {
+    codigo = new Codigo();
+    estado = SUCESSO;
+};
+void TesteCodigo::tearDown() {
+    delete codigo;
+};
+void TesteCodigo::testarValorInvalido(string valor) {
+    try {
+        codigo->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (codigo->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+void TesteCodigo::testarValorValido(string valor) {
+    try {
+        codigo->setValor(valor);
+        if (codigo->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+string TesteCodigo::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+};
+
+void TesteMatricula::setUp() {
+    matricula = new Matricula();
+    estado = SUCESSO;
+};
+void TesteMatricula::tearDown() {
+    delete matricula;
+};
+void TesteMatricula::testarValorInvalido(string valor) {
+    try {
+        matricula->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (matricula->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+void TesteMatricula::testarValorValido(string valor) {
+    try {
+        matricula->setValor(valor);
+        if (matricula->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+string TesteMatricula::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+};
+
+void TesteResultado::setUp() {
+    resultado = new Resultado();
+    estado = SUCESSO;
+};
+void TesteResultado::tearDown() {
+    delete resultado;
+};
+void TesteResultado::testarValorInvalido(string valor) {
+    try {
+        resultado->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (resultado->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+void TesteResultado::testarValorValido(string valor) {
+    try {
+        resultado->setValor(valor);
+        if (resultado->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+string TesteResultado::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+};
+
+void TesteTexto::setUp() {
+    texto = new Texto();
+    estado = SUCESSO;
+};
+void TesteTexto::tearDown() {
+    delete texto;
+};
+void TesteTexto::testarValorInvalido(string valor) {
+    try {
+        texto->setValor(valor);
+        estado = FALHA + "Valor invalido setado";
+    }
+    catch(invalid_argument &excecao) {
+        if (texto->getValor() == valor) {
+            estado = FALHA + excecao.what();
+        };
+    };
+};
+void TesteTexto::testarValorValido(string valor) {
+    try {
+        texto->setValor(valor);
+        if (texto->getValor() != valor) {
+            estado = FALHA + "Valor valido não foi setado";
+        };
+    }
+    catch(invalid_argument &excecao) {
+        estado = FALHA + excecao.what();
+    };
+};
+string TesteTexto::run(string valorInvalido, string valorValido) {
+    setUp();
+    testarValorInvalido(valorInvalido);
+    testarValorValido(valorValido);
+    tearDown();
+    return estado;
+};
--- testes.hpp
+++ testes.hpp
@@ -56,4 +56,100 @@ public:
     string run(string, string);
 };
 
+class TesteClasse {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Classe *classe;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
+
+class TesteTelefone {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Telefone *telefone;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
+  
+class TesteCodigo {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Codigo *codigo;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
+
+class TesteMatricula {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Matricula *matricula;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
+
+class TesteResultado {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Resultado *resultado;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
+
+class TesteTexto {
+private:
+    // const static string VALOR_INVALIDO;
+    // const static string VALOR_VALIDO;
+    Texto *texto;
+    string estado;
+    void setUp();
+    void tearDown();
+    void testarValorInvalido(string);
+    void testarValorValido(string);
+public:
+    const string SUCESSO = "Sucesso!";
+    const string FALHA = "Erro: ";
+    string run(string, string);
+};
+
 #endif // TESTES_HPP_INCLUDED
