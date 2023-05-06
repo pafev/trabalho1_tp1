de010a440fb929777eb940ad314b3055a389c527
pafev <paulofvilarimda@gmail.com>
2023-05-05 22:44:48 -0300

    teste

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include "dominios.hpp"
-#include "entidades.hpp"
-#include "testes.hpp"
+#include "dominios/dominios.hpp"
+#include "entidades/entidades.hpp"
+#include "testes/testes.hpp"
 
 #include <iostream>
 using namespace std;
@@ -8,7 +8,11 @@ using namespace std;
 int main() {
     Data x;
     x.setValor("30/FEV/2004");
-
     cout << x.getValor() << endl;
+
+    Data z;
+    z.setValor("01/DEZ/2999");
+    cout << z.getValor() << endl;
+
     return 0;
 }
