a3faeb6177778934b9645cceea5d9c6df0813c59
carollingian <carolina.fdcampos@gmail.com>
2023-05-13 23:41:32 -0300

    teste de entidades

--- testes.cpp
+++ testes.cpp
@@ -1,47 +1,7 @@
 #include "testes.hpp"
 
-// void TesteDominio::setUp() {
-//     dominio = new Dominio();
-//     estado = SUCESSO;
-// };
 
-// void TesteDominio::tearDown() {
-//     delete dominio;
-// };
-
-// void TesteDominio::testarValorInvalido(string valor) {
-//     try {
-//         dominio->setValor(valor);
-//         estado = FALHA + "Valor invalido setado";
-//     }
-//     catch(invalid_argument &excecao) {
-//         if (dominio->getValor() == valor) {
-//             estado = FALHA + excecao.what();
-//         };
-//     };
-// };
-
-// void TesteDominio::testarValorValido(string valor) {
-//     try {
-//         dominio->setValor(valor);
-//         if (dominio->getValor() != valor) {
-//             estado = FALHA + "Valor valido não foi setado";
-//         };
-//     }
-//     catch(invalid_argument &excecao) {
-//         estado = FALHA + excecao.what();
-//     };
-// };
-
-// string TesteDominio::run(string valorInvalido, string valorValido) {
-//     setUp();
-//     testarValorInvalido(valorInvalido);
-//     testarValorValido(valorValido);
-//     tearDown();
-//     return estado;
-// }
-
-
+//Testes de Dominio
 void TesteData::setUp() {
     data = new Data();
     estado = SUCESSO;
@@ -198,28 +158,28 @@ string TesteTelefone::run(string valorInvalido, string valorValido) {
     return estado;
 };
 
-void TesteCodigo::setUp() {
-    codigo = new Codigo();
+void TesteMatricula::setUp() {
+    matricula = new Matricula();
     estado = SUCESSO;
 };
-void TesteCodigo::tearDown() {
-    delete codigo;
+void TesteMatricula::tearDown() {
+    delete matricula;
 };
-void TesteCodigo::testarValorInvalido(string valor) {
+void TesteMatricula::testarValorInvalido(string valor) {
     try {
-        codigo->setValor(valor);
+        matricula->setValor(valor);
         estado = FALHA + "Valor invalido setado";
     }
     catch(invalid_argument &excecao) {
-        if (codigo->getValor() == valor) {
+        if (matricula->getValor() == valor) {
             estado = FALHA + excecao.what();
         };
     };
 };
-void TesteCodigo::testarValorValido(string valor) {
+void TesteMatricula::testarValorValido(string valor) {
     try {
-        codigo->setValor(valor);
-        if (codigo->getValor() != valor) {
+        matricula->setValor(valor);
+        if (matricula->getValor() != valor) {
             estado = FALHA + "Valor valido não foi setado";
         };
     }
@@ -227,7 +187,7 @@ void TesteCodigo::testarValorValido(string valor) {
         estado = FALHA + excecao.what();
     };
 };
-string TesteCodigo::run(string valorInvalido, string valorValido) {
+string TesteMatricula::run(string valorInvalido, string valorValido) {
     setUp();
     testarValorInvalido(valorInvalido);
     testarValorValido(valorValido);
@@ -345,3 +305,139 @@ string TesteTexto::run(string valorInvalido, string valorValido) {
     tearDown();
     return estado;
 };
+
+
+// Testes de Entidade
+void TesteDesenvolvedor::setUp(){
+    desenvolvedor = new Desenvolvedor();
+    estado = SUCESSO;
+}
+
+void TesteDesenvolvedor::tearDown(){
+    delete desenvolvedor;
+}
+
+void TesteDesenvolvedor::testarCenarioSucesso(){
+    Matricula matricula;
+    matricula.setValor(MATRICULA_VALIDA);
+    desenvolvedor->setMatricula(matricula);
+    if(desenvolvedor->getMatricula().getValor() != MATRICULA_VALIDA)
+        estado = FALHA;
+
+    Texto nome;
+    nome.setValor(NOME_VALIDO);
+    desenvolvedor->setNome(nome);
+    if(desenvolvedor->getNome().getValor() != NOME_VALIDO)
+        estado = FALHA;
+
+    Senha senha;
+    senha.setValor(SENHA_VALIDA);
+    desenvolvedor->setSenha(senha);
+    if(desenvolvedor->getSenha().getValor() != SENHA_VALIDA)
+        estado = FALHA;
+
+    Telefone telefone;
+    telefone.setValor(TELEFONE_VALIDO);
+    desenvolvedor->setTelefone(telefone);
+    if(desenvolvedor->getTelefone().getValor() != TELEFONE_VALIDO)
+        estado = FALHA;
+}
+
+int TesteDesenvolvedor::run(){
+    setUp();
+    testarCenarioSucesso();
+    tearDown();
+    return estado;
+}
+
+
+void TesteTeste::setUp(){
+    teste = new Teste();
+    estado = SUCESSO;
+}
+
+void TesteTeste::tearDown(){
+    delete teste;
+}
+
+void TesteTeste::testarCenarioSucesso(){
+    Codigo codigo;
+    codigo.setValor(CODIGO_VALIDO);
+    teste->setCodigo(codigo);
+    if(teste->getCodigo().getValor() != CODIGO_VALIDO)
+        estado = FALHA;
+
+    Texto nome;
+    nome.setValor(NOME_VALIDO);
+    teste->setNome(nome);
+    if(teste->getNome().getValor() != NOME_VALIDO)
+        estado = FALHA;
+    
+    Classe classe;
+    classe.setValor(CLASSE_VALIDA);
+    teste->setClasse(classe);
+    if(teste->getClasse().getValor() != CLASSE_VALIDA)
+        estado = FALHA;
+}
+
+int TesteTeste::run(){
+    setUp();
+    testarCenarioSucesso();
+    tearDown();
+    return estado;
+}
+
+
+void TesteCasoDeTeste::setUp(){
+    casodeteste = new CasoDeTeste();
+    estado = SUCESSO;
+}
+
+void TesteCasoDeTeste::tearDown(){
+    delete casodeteste;
+}
+
+void TesteCasoDeTeste::testarCenarioSucesso(){
+    Codigo codigo;
+    codigo.setValor(CODIGO_VALIDO);
+    casodeteste->setCodigo(codigo);
+    if(casodeteste->getCodigo().getValor() != CODIGO_VALIDO)
+        estado = FALHA;
+
+    Texto nome;
+    nome.setValor(NOME_VALIDO);
+    casodeteste->setNome(nome);
+    if(casodeteste->getNome().getValor() != NOME_VALIDO)
+        estado = FALHA;
+
+    Data data;
+    data.setValor(DATA_VALIDA);
+    casodeteste->setData(data);
+    if(casodeteste->getData().getValor() != DATA_VALIDA)
+        estado = FALHA;
+
+    Texto acao;
+    acao.setValor(ACAO_VALIDA);
+    casodeteste->setAcao(acao);
+    if(casodeteste->getAcao().getValor() != ACAO_VALIDA)
+        estado = FALHA;
+
+    Texto resposta;
+    resposta.setValor(RESPOSTA_VALIDA);
+    casodeteste->setResposta(resposta);
+    if(casodeteste->getResposta().getValor() != RESPOSTA_VALIDA)
+        estado = FALHA;
+
+    Resultado resultado;
+    resultado.setValor(RESULTADO_VALIDO);
+    casodeteste->setResultado(resultado);
+    if(casodeteste->getResultado().getValor() != RESULTADO_VALIDO)
+        estado = FALHA;
+}
+
+int TesteCasoDeTeste::run(){
+    setUp();
+    testarCenarioSucesso();
+    tearDown();
+    return estado;
+}
