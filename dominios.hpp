5183fa92b07ee9718b637046ef2084721e22ff0b
pafev <paulofvilarimda@gmail.com>
2023-05-13 23:27:11 -0300

    adjusting class Dominio

--- dominios.hpp
+++ dominios.hpp
@@ -6,9 +6,12 @@
 using namespace std;
 
 class Dominio {
+private:
+    virtual void validar(string) = 0
 protected:
     string valor;
 public:
+    virtual void setValor(string) = 0;
     string getValor() const;
 };
 
